1856ecb0cb9a5c94c10d40dc19546e833a7c5011
Hrystyna Yaskulska <yaskulskahrystyna@gmail.com>
2023-10-11 20:49:20 +0300

    Add method calculate

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,10 +2,9 @@ package org.example;
 
 public class App {
   public static void main(String[] args) {
-    MatrixCalculator matrixCalculator = new MatrixCalculator(6,6);
+    MatrixCalculator matrixCalculator = new MatrixCalculator(3,3);
     matrixCalculator.setMatrixA((short)-30,(short)30);
     matrixCalculator.setMatrixB((short)-40,(short)40);
-
-
+    matrixCalculator.calculate();
   }
 }
--- src/main/java/org/example/MatrixCalculator.java
+++ src/main/java/org/example/MatrixCalculator.java
@@ -1,25 +1,28 @@
 package org.example;
 
 public class MatrixCalculator {
-    private int m;
-    private int n;
-    private short matrixA[][];
-    private short matrixB[][];
+    private final int m;
+    private final int n;
+    private int sum = 0;
+    private final short[][] matrixA;
+    private final short[][] matrixB;
+    private final short[][] matrixC;
     public MatrixCalculator(int m, int n) {
         this.m = m;
         this.n = n;
         this.matrixA = new short[m][n];
         this.matrixB = new short[m][n];
+        this.matrixC = new short[m][n];
     }
 
-    private void setMatrix(short matrix[][], short min, short max){
+    private void setMatrix(short[][] matrix, short min, short max){
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                matrix[i][j] = (short)((Math.random()*(max - min + 1)) + min);
             }
         }
     }
-    private void printMatrix(short matrix[][]){
+    private void printMatrix(short[][] matrix){
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 System.out.print(matrix[i][j] + " ");
@@ -38,7 +41,19 @@ public class MatrixCalculator {
         System.out.println("Matrix B:");
         printMatrix(matrixB);
     }
-    
-
-
+    public void calculate(){
+        for (int i = 0; i < m; i++) {
+            int minVal = matrixA[i][0] + matrixB[i][0];
+            for (int j = 0; j < n; j++) {
+                matrixC[i][j] = (short)(matrixA[i][j] + matrixB[i][j]);
+                if (minVal > matrixC[i][j]) {
+                    minVal = matrixC[i][j];
+                }
+            }
+            sum += minVal;
+        }
+        System.out.println("A + B:");
+        printMatrix(matrixC);
+        System.out.print("Sum = " + sum);
+    }
 }
